a8553cb0bb67a4668fefe453eea093d9a990fccc
alex-Arc <alex@arc-light.dk>
2018-04-13 14:57:54 +0200

    implement sorting

--- server.js
+++ server.js
@@ -18,6 +18,62 @@ let mode = 'live'
 // let showSubNodes = []
 // var abc = ['A', 'B', 'C', 'D']
 
+let sortType = 'none'
+
+$('#addrSort').on('click', () => {
+  logger.verbose({button: 'addrSort'})
+  let addrSortBtn = document.getElementById('addrSort')
+  let statusSortBtn = document.getElementById('statusSort')
+  let nameSortBtn = document.getElementById('nameSort')
+  addrSortBtn.className = 'btn btn-primary btn-xs'
+  statusSortBtn.className = 'btn btn-default btn-xs'
+  nameSortBtn.className = 'btn btn-default btn-xs'
+  if (sortType === 'addrUp') {
+    sortType = 'addrDn'
+    addrSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-order-alt" aria-hidden="true"> </span>'
+  } else {
+    sortType = 'addrUp'
+    addrSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-order" aria-hidden="true"> </span>'
+  }
+  drawTable()
+})
+
+$('#statusSort').on('click', () => {
+  logger.verbose({button: 'statusSort'})
+  let addrSortBtn = document.getElementById('addrSort')
+  let statusSortBtn = document.getElementById('statusSort')
+  let nameSortBtn = document.getElementById('nameSort')
+  addrSortBtn.className = 'btn btn-default btn-xs'
+  statusSortBtn.className = 'btn btn-primary btn-xs'
+  nameSortBtn.className = 'btn btn-default btn-xs'
+  if (sortType === 'statusUp') {
+    sortType = 'statusDn'
+    statusSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-attributes-alt" aria-hidden="true"> </span>'
+  } else {
+    sortType = 'statusUp'
+    statusSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-attributes" aria-hidden="true"> </span>'
+  }
+  drawTable()
+})
+
+$('#nameSort').on('click', () => {
+  logger.verbose({button: 'nameSort'})
+  let addrSortBtn = document.getElementById('addrSort')
+  let statusSortBtn = document.getElementById('statusSort')
+  let nameSortBtn = document.getElementById('nameSort')
+  addrSortBtn.className = 'btn btn-default btn-xs'
+  statusSortBtn.className = 'btn btn-default btn-xs'
+  nameSortBtn.className = 'btn btn-primary btn-xs'
+  if (sortType === 'nameUp') {
+    sortType = 'nameDn'
+    nameSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-alphabet-alt" aria-hidden="true"> </span>'
+  } else {
+    sortType = 'nameUp'
+    nameSortBtn.innerHTML = '<span class="glyphicon glyphicon-sort-by-alphabet" aria-hidden="true"> </span>'
+  }
+  drawTable()
+})
+
 $('#update-table').on('click', () => {
   logger.verbose({button: 'update-table'})
   updateTable()
@@ -122,6 +178,16 @@ function updateTable () {
   }
 }
 
+function compareStatusUp (a, b) {
+  if (a.status === 'Online' && b.status === 'Offline') {
+    return -1
+  }
+  if (a.status === 'Offline' && b.status === 'Online') {
+    return 1
+  }
+  return 0
+}
+
 function compareMac (a, b) {
   if (a.mac < b.mac) {
     return -1
@@ -132,7 +198,7 @@ function compareMac (a, b) {
   return 0
 }
 
-function compareName (a, b) {
+function compareNameUp (a, b) {
   if (a.name < b.name) {
     return -1
   }
@@ -141,14 +207,35 @@ function compareName (a, b) {
   }
   return 0
 }
+function compareNameDn (a, b) {
+  if (a.name > b.name) {
+    return -1
+  }
+  if (a.name < b.name) {
+    return 1
+  }
+  return 0
+}
 
-function compareAddr (a, b) {
-  let c = a.net << 8 + a.sub
-  let d = a.net << 8 + a.sub
+function compareAddrUp (a, b) {
+  let c = (a.net << 8) + (a.sub << 4) + a.univers[0]
+  let d = (b.net << 8) + (b.sub << 4) + b.univers[0]
   if (c < d) {
     return -1
   }
   if (c > d) {
+    return 1
+  }
+  return 0
+}
+
+function compareAddrDn (a, b) {
+  let c = (a.net << 8) + (a.sub << 4) + a.univers[0]
+  let d = (b.net << 8) + (b.sub << 4) + b.univers[0]
+  if (c > d) {
+    return -1
+  }
+  if (c < d) {
     return 1
   }
   return 0
@@ -161,7 +248,29 @@ function drawTable () {
   table.innerHTML = ''
   if (node.length > 0) {
     Dnode = node
-    Dnode.sort(compareName)
+    switch (sortType) {
+      case 'statusUp':
+        Dnode.sort(compareStatusUp)
+        break
+      case 'statusDn':
+        Dnode.sort(compareStatusUp)
+        break
+      case 'nameUp':
+        Dnode.sort(compareNameUp)
+        break
+      case 'nameDn':
+        Dnode.sort(compareNameDn)
+        break
+      case 'addrUp':
+        Dnode.sort(compareAddrUp)
+        break
+      case 'addrDn':
+        Dnode.sort(compareAddrDn)
+        break
+      case 'none':
+        break
+    }
+
     // Dnode.reverse()
     for (var i = 0; i < Dnode.length; i++) {
       if (Dnode[i].mac !== undefined) {
