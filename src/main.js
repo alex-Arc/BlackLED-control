5478598c76d4bc40488359bc0b9d4f3cef2cac32
alex-Arc <alex@arc-light.dk>
2018-09-09 13:03:44 +0200

    fix paceger

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 const {app, BrowserWindow} = require('electron')
-const remote = require('electron').remote
 const url = require('url')
 const path = require('path')
 
-require('electron-reload')(__dirname)
+var env = process.argv[2]
+
+if (env === 'dev') {
+  require('electron-reload')(__dirname)
+}
 
 let mainWindow
-
-// console.log('test 1 2 3!')
-// console.log(process.env.NODE_ENV)
 
 let logFolder = path.join(app.getPath('userData'), 'logs')
 let logFile = path.join(logFolder, new Date().toISOString().replace(/:/g, '.') + '.log')
 
 const logger = require('winston')
 logger.clear()
-// if (process.env.NODE_ENV !== 'production') {
-logger.add(logger.transports.Console, {colorize: true, level: 'error'}) //  }
+
+if (env === 'dev') {
+  logger.add(logger.transports.Console, {colorize: true, level: 'verbose'})
+} else {
+  logger.add(logger.transports.Console, {colorize: true, level: 'error'})
+}
+
+logger.verbose(env)
 
 global.logger = logger
 
-function startUp () {
+app.on('ready', function () {
   logger.add(logger.transports.File, { filename: logFile, level: 'info', logstash: true })
-  createWindow()
-  // require('./menu/mainmenu')
-}
-
-function createWindow () {
   mainWindow = new BrowserWindow({width: 900, height: 950})
   mainWindow.eval = global.eval = function () {
     throw new Error(`Sorry, this app does not support window.eval().`)
@@ -38,9 +39,7 @@ function createWindow () {
     logger.verbose('window closed')
     mainWindow = null
   })
-}
-
-app.on('ready', startUp)
+})
 
 app.on('will-quit', function () {
   logger.verbose('will-quit')
@@ -53,10 +52,4 @@ app.on('window-all-closed', function () {
 
 app.on('before-quit', function () {
   logger.verbose('before-quit')
-})
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
 })
